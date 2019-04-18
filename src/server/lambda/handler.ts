fc84b1d7bb01a87f5d851cd6c81c8c1d9ebd6e0e
cbhubbard <hubbard8@gmail.com>
2019-04-18 13:39:42 -0400

    Added code to run the ecs task -- needs to be moved to lambda services

--- src/server/lambda/handler.ts
+++ src/server/lambda/handler.ts
@@ -1,17 +1,61 @@
-import { Handler, APIGatewayProxyEvent, Context } from 'aws-lambda';
+import { Handler, APIGatewayProxyEvent, Context, APIGatewayProxyResult } from 'aws-lambda';
+import { ECS, AWSError } from 'aws-sdk';
 
-export const createConnector: Handler = (event: APIGatewayProxyEvent, context: Context) =>
+// Use an environment variable for the current docker container tag for the task definition
+const CURRENT_DOCKER_TAG: string = '1';
+
+// Create an ECS context to create tasks
+const ecs: ECS = new ECS();
+
+export const createConnector: Handler = async (event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> =>
 {
     console.log(event);
     console.log(context);
 
-    return
+    // Create a ECS Fargate task -- the configuration here will likely need to be changed
+    const runTaskParams: ECS.RunTaskRequest = {
+        taskDefinition: `ilp-cloud-connector:${CURRENT_DOCKER_TAG}`,
+        launchType: 'FARGATE',
+        cluster: 'ILP-Cloud-Connectors',
+    };
+
+    // Lambda needs to have ECS IAM policies
+    const task = ecs.runTask(runTaskParams, (error: AWSError, data: ECS.RunTaskResponse) =>
+    {
+        // Check for errors during the creation of this ECS Fargate task
+        if (error)
+        {
+            console.error(error, error.stack);
+        }
+        else
+        {
+            // Place the information on this connector's task into the Table
+            console.log('Data from ECS task creation...');
+            console.log(data);
+        }
+    });
+
+    console.log(task);
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: 'Hello World'
+        })
+    };
 }
 
-export const getConnector: Handler = (event: APIGatewayProxyEvent, context: Context) =>
+export const getConnector: Handler = async (event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> =>
 {
     console.log(event);
     console.log(context);
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: 'Hello World'
+        })
+    };
 }
 
 export const deleteConnector: Handler = (event: APIGatewayProxyEvent, context: Context) =>
